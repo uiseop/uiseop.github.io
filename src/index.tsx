70a205ab7c23228e1f1adfe5c59a33237dec921e
uiseop <bono521@naver.com>
2023-11-23 17:46:41 +0900

    ADD: React 17버전 이후 import React from 'react'없이 설정할 수 있도록 eslint rules 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import App from "./App";
-import { createRoot } from "react-dom/client";
+import App from './App';
+import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container as Element);
 
 root.render(<App />);
