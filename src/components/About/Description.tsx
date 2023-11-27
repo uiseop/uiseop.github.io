5107766e03668e233910421b03a9b4a57a859f57
uiseop <bono521@naver.com>
2023-11-27 16:32:48 +0900

    ADD: 전역변수 -> useRef로 변경, cursor height 변겅

--- src/components/About/Description.tsx
+++ src/components/About/Description.tsx
@@ -13,40 +13,39 @@ const WORDS = [
 const writeSpeed = 100;
 const deleteSpeed = 50;
 const changeWordSpeed = 1000;
-let wordIndex = 0;
-let charIndex = 1;
-let isDeleting = false;
 
 const Description: FunctionComponent = () => {
 	const cursorRef = useRef<HTMLSpanElement>(null);
 	let timer: ReturnType<typeof setTimeout>;
+	let wordIndex = useRef(0);
+	let charIndex = useRef(1);
+	let isDeleting = useRef(false);
 
 	useEffect(() => {
 		if (cursorRef.current) {
 			timer = setTimeout(typeEffect, 500);
 		}
 		return () => {
-			wordIndex = 0;
-			charIndex = 0;
-			isDeleting = false;
 			clearTimeout(timer);
 		};
 	}, []);
 
 	function typeEffect() {
-		const currentWord = WORDS[wordIndex];
-		const currentChar = currentWord.slice(0, charIndex);
+		const currentWord = WORDS[wordIndex.current];
+		const currentChar = currentWord.slice(0, charIndex.current);
 		cursorRef.current!.textContent = currentChar;
 
-		if (!isDeleting && charIndex < currentWord.length) {
-			charIndex += 1;
+		if (!isDeleting.current && charIndex.current < currentWord.length) {
+			charIndex.current += 1;
 			timer = setTimeout(typeEffect, writeSpeed);
-		} else if (isDeleting && charIndex > 0) {
-			charIndex -= 1;
+		} else if (isDeleting.current && charIndex.current > 0) {
+			charIndex.current -= 1;
 			timer = setTimeout(typeEffect, deleteSpeed);
 		} else {
-			isDeleting = !isDeleting;
-			wordIndex = !isDeleting ? (wordIndex + 1) % WORDS.length : wordIndex;
+			isDeleting.current = !isDeleting.current;
+			wordIndex.current = !isDeleting.current
+				? (wordIndex.current + 1) % WORDS.length
+				: wordIndex.current;
 			timer = setTimeout(typeEffect, changeWordSpeed);
 		}
 	}
@@ -74,8 +73,6 @@ const CursorSpan = styled.span({
 		content: '""',
 		position: 'absolute',
 		right: '-6px',
-		top: '50%',
-		transform: 'translateY(-50%)',
 		height: '3rem',
 		backgroundColor: theme.colors.background,
 		borderLeft: `3px solid ${theme.colors.text}`,
