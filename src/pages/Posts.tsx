e57f85107d840f182c7a4833a4534e878f52025e
uiseop <bono521@naver.com>
2023-12-14 13:43:49 +0900

    EDIT: static/index에서 category 분류

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -2,23 +2,7 @@ import styled from '@emotion/styled';
 import { theme } from '@components/common/theme';
 import { Navigationbar, PostList } from '@components/Posts';
 import { Header } from '@components/common/Header';
-import { files } from '@static/index';
-import { CustomGrayMatterFile } from '@components/common';
-import matter from 'gray-matter';
-
-let categorySet = new Set<string>();
-let categories: string[];
-
-function handleCategories() {
-	files.forEach(({ file }) => {
-		const { data } = matter(file) as CustomGrayMatterFile;
-		data.categories.forEach((category) => categorySet.add(category));
-	});
-
-	categories = Array.from(categorySet);
-}
-
-handleCategories();
+import { categories, files } from '@static/index';
 
 const Posts = () => {
 	return (
--- src/static/index.ts
+++ src/static/index.ts
@@ -2,8 +2,24 @@ import test from './test.md';
 import test2 from './test copy.md';
 import test3 from './test copy 2.md';
 import { MdFile } from 'types';
+import matter from 'gray-matter';
+import { CustomGrayMatterFile } from '@components/common';
 
 export const files: MdFile[] = [test, test2, test3].map((file, idx) => ({
 	file,
 	key: idx,
 }));
+
+let categorySet = new Set<string>();
+export let categories: string[];
+
+function handleCategories() {
+	files.forEach(({ file }) => {
+		const { data } = matter(file) as CustomGrayMatterFile;
+		data.categories.forEach((category) => categorySet.add(category));
+	});
+
+	categories = Array.from(categorySet);
+}
+
+handleCategories();
