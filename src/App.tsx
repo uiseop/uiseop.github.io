e1b35c40456a868068a70e44b9dbe0c34d9188f7
uiseop <bono521@naver.com>
2023-12-24 00:18:39 +0900

    ADD: GA4 설치

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,33 @@ import {
 	ToastContainer,
 } from '@components/common';
 import { Routes } from './Routes';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ga4 from 'react-ga4';
+
+let init = false;
 
 const App = () => {
+	const location = useLocation();
+
+	useEffect(() => {
+		if (!init) {
+			init = true;
+			ga4.initialize('G-HZBWRN05E4');
+		}
+
+		return () => {
+			init = false;
+		};
+	}, []);
+
+	useEffect(() => {
+		ga4.send({
+			hitType: 'pageview',
+			page: location.pathname,
+		});
+	}, [location]);
+
 	return (
 		<ThemeProvider>
 			<GlobalStyle />
